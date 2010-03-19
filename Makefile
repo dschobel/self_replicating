3e7d3f222975af4f9ceb39a63a363b55a236a512
dschobel <dschobel@gmail.com>
2010-03-18 23:40:26 -0500

    poking around naive solution

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
-CC=gcc
+CC=g++
 CFLAGS=-Wall
 
 all: self
+	./self
+
+clean:
+	rm self
 
 self:
 	$(CC) $(CFLAGS) self.c -o self
--- self.c
+++ self.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world!\n");
+    const char *header = "#include <stdio.h>\n\nint main(){\n";
+    const char *body = "int main(){ \n const char *header = \"#include <stdio.h>\\n\"";
+    const char *body2 = "\n\tprintf(header);\n\tprintf(body);\n\treturn 0;\n}\n";
+    printf(header);
+    printf(body2);
     return 0;
 }
